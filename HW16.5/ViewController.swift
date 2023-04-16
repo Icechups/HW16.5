ea8bc189fd93a9d2cf78140b340ffac024c437aa
Ilya Perevozkin <Perevozkin01@icloud.com>
2023-04-16 12:31:00 +0500

    first commit

---
+++ HW16.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW16.5
+//
+//  Created by Илья Перевозкин on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
